6a432ee73bcb48f21f6dd6d107279f328f540dc3
firsachi <firsachi@firsbook>
2020-05-13 15:40:55 +0300

    Creating method name column

--- callcenter/src/main/java/callcenter/filter/MapColumns.java
+++ callcenter/src/main/java/callcenter/filter/MapColumns.java
@@ -4,8 +4,11 @@
 package callcenter.filter;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
 import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
 /**
@@ -13,18 +16,30 @@ import org.apache.poi.xwpf.usermodel.XWPFTableRow;
  *
  */
 public class MapColumns {
-	
-	private final Map<DefaultNameColumn, NameColumn> filter;
-	
+
+	private Map<DefaultNameColumn, NameColumn> resultMap;
 
 	public MapColumns() {
 		super();
-		this.filter = new HashMap<DefaultNameColumn, NameColumn>();
+		this.resultMap = new HashMap<>();
 	}
 
-
-	public Map<DefaultNameColumn, NameColumn> mapColumn(XWPFTableRow row) {
-		return filter;
+	public Map<DefaultNameColumn, NameColumn> mapColumn(XWPFTableRow row) throws Exception {
+		List<XWPFTableCell> cells = row.getTableCells();
+		if (DefaultNameColumn.values().length <= cells.size()) {
+			cells.stream()
+					.forEach(cell -> Stream.of(DefaultNameColumn.values())
+							.filter(nameCell -> cell.getText().trim().toLowerCase().equals(nameCell.getName()))
+							.findAny().map(name -> resultMap.put(name, new NameColumn(cells.indexOf(cell), name))));
+		} else {
+			// TO DO log
+			throw new Exception();
+		}
+		if (DefaultNameColumn.values().length != resultMap.size()) {
+			// TO DO log
+			throw new Exception();
+		}
+		return resultMap;
 	}
 
 }
