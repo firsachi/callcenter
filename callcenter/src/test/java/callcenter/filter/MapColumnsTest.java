5b1339b36b196e460befdc8e9b937afe80606a4e
firsov <firsov@firsov.kievreclama.local>
2020-05-26 16:19:41 +0300

    add method tearDown

--- callcenter/src/test/java/callcenter/filter/MapColumnsTest.java
+++ callcenter/src/test/java/callcenter/filter/MapColumnsTest.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,8 +17,17 @@ class MapColumnsTest {
 	
 	private Map<DefaultNameColumn, NameColumn> expected;
 	
+	private FileInputStream fis;
+	
+	private XWPFDocument document;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@BeforeEach
-    void init() {
+    void init() throws Exception {
+		fis = new FileInputStream("src/test/resources/test_filter.docx");
+		document = new XWPFDocument(OPCPackage.open(fis));
 		expected = new HashMap<DefaultNameColumn, NameColumn>();
 		expected.put(DefaultNameColumn.NUMBER, new NameColumn(1, DefaultNameColumn.NUMBER));
 		expected.put(DefaultNameColumn.DATEDUE, new NameColumn(2, DefaultNameColumn.DATEDUE));
@@ -25,17 +35,23 @@ class MapColumnsTest {
 		expected.put(DefaultNameColumn.CORRESPONDENT, new NameColumn(4, DefaultNameColumn.CORRESPONDENT));
 		expected.put(DefaultNameColumn.SUMMARY, new NameColumn(5, DefaultNameColumn.SUMMARY));
     }
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+		document.close();
+		fis.close();
+	}
 
 	@Test
 	void testMapColumn() throws Exception {
-		FileInputStream fis = new FileInputStream("src/test/resources/test_filter.docx");
-		XWPFDocument document = new XWPFDocument(OPCPackage.open(fis));
 		XWPFTable table = document.getTables().get(0);
 		MapColumns mapColumns = new MapColumns();
 		Map<DefaultNameColumn, NameColumn> actual = mapColumns.mapColumn(table.getRow(0));
 		assertEquals(expected, actual);
-		document.close();
-		fis.close();
+		
 	}
 	
 	@Test
@@ -45,14 +61,10 @@ class MapColumnsTest {
 		XWPFTable table = document.getTables().get(1);
 		MapColumns mapColumns = new MapColumns();
 		assertThrows(Exception.class, () -> mapColumns.mapColumn(table.getRow(0)));
-		document.close();
-		fis.close();
 	}
 	
 	@Test
 	void testNumberColumnsTable() throws Exception {
-		FileInputStream fis = new FileInputStream("src/test/resources/test_filter.docx");
-		XWPFDocument document = new XWPFDocument(OPCPackage.open(fis));
 		XWPFTable table = document.getTables().get(0);
 		table.getRow(0).removeCell(0);
 		table.getRow(0).removeCell(0);
@@ -61,32 +73,22 @@ class MapColumnsTest {
 		table.getRow(0).removeCell(0);
 		MapColumns mapColumns = new MapColumns();
 		assertThrows( Exception.class, () ->  mapColumns.mapColumn(table.getRow(0)));
-		document.close();
-		fis.close();
 	}
 
 	@Test
 	void testCheckSize() throws Exception {
-		FileInputStream fis = new FileInputStream("src/test/resources/test_filter.docx");
-		XWPFDocument document = new XWPFDocument(OPCPackage.open(fis));
 		XWPFTable table = document.getTables().get(0);
 		MapColumns mapColumns = new MapColumns();
 		assertEquals(DefaultNameColumn.values().length, mapColumns.mapColumn(table.getRow(0)).size());
-		document.close();
-		fis.close();
 	}
 	
 	@Test
 	void testCheckSizeIncorrect() throws Exception {
-		FileInputStream fis = new FileInputStream("src/test/resources/test_filter.docx");
-		XWPFDocument document = new XWPFDocument(OPCPackage.open(fis));
 		XWPFTable table = document.getTables().get(0);
 		table.getRow(0).removeCell(0);
 		table.getRow(0).removeCell(0);
 		table.getRow(0).removeCell(0);
 		MapColumns mapColumns = new MapColumns();
 		assertThrows( Exception.class, () ->  mapColumns.mapColumn(table.getRow(0)));
-		document.close();
-		fis.close();
 	}
 }
